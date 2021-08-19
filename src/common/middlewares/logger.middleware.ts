e8f9d6f2c71aac2050af81f5cadc1909db57b1b2
SeongHyeon Ji <kitys2k3@naver.com>
2021-08-19 23:53:26 +0900

    공통 filter, interceptor, middleware 생성

---
+++ src/common/middlewares/logger.middleware.ts
@@ -0,0 +1,19 @@
+import { Injectable, Logger, NestMiddleware } from '@nestjs/common';
+import { NextFunction, Request, Response } from 'express';
+
+@Injectable()
+export class LoggerMiddleware implements NestMiddleware {
+  private logger = new Logger();
+
+  use(req: Request, res: Response, next: NextFunction) {
+    const { ip, method, originalUrl } = req;
+
+    this.logger.log(`요청 : ${originalUrl} ${method}`, ip);
+
+    res.on('finish', () => {
+      this.logger.log(`응답 : ${res.statusCode}`, ip);
+    });
+
+    next();
+  }
+}
