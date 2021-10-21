2db97e6e8e63b27e80341b4b55e193666abc6af2
SeongHyeon Ji <j.eq.h0507@gmail.com>
2021-10-21 18:41:01 +0900

    cors 설정 추가

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,9 @@ import { logger } from './middleware/logger.middleware';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: [`http://localhost:${process.env.PORT}`, 'https://honeyjee.com'],
+  });
   app.use(logger);
   app.setGlobalPrefix('api');
   app.useGlobalInterceptors(new LoggingInterceptor());
